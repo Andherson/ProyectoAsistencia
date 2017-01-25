0a35110b584156d2120ca822197c52e2f7c19cc3
Karma <andcristhian95@gmail.com>
2017-01-25 03:41:18 -0500

    Registro y login adm

--- funciones.php
+++ funciones.php
@@ -2,23 +2,58 @@
 
 require 'conexion.php';
 
-function registrarEntrada($codbus,$tiempo) {
 
-    $datosUsuario = array();
+
+
+function obtenerUsuario($codbus){
 
     $conexion = conexion();
-    $string1 = "SELECT * from usuario where cod_usuario = $codbus";
-    $string2 = "INSERT INTO asistencia (id_asistencia,timein,timeout,cod_usuario)
-                values('','$tiempo','','$codbus')";
-    $consulta = mysqli_query($conexion, $string1) or die('error en la consulta'.mysqli_connect_error());
+    $strusu = "SELECT * FROM usuario WHERE cod_usuario = $codbus";
 
-    $datosUsuario = mysqli_fetch_assoc($consulta);
+    $conusu = mysqli_query($conexion,$strusu) or die('error en la consulta'.mysqli_connect_error());
+    $datosUsuario = mysqli_fetch_assoc($conusu);
 
-    if(!is_null($datosUsuario)){
-        mysqli_query($conexion, $string2) or die('error en la consulta'.mysqli_connect_error());
-        return $datosUsuario;
-    }else
-        return ;
+    return $datosUsuario;
 
 }
 
+
+
+
+function registrar($codreg,$tiempo){
+
+
+    $conexion = conexion();
+    $usuario = obtenerUsuario($codreg);
+
+    if(is_null($usuario)){ // usuario no registrado
+        return -1;
+    }else{
+        $strasis= "SELECT id_asistencia FROM asistencia WHERE cod_usuario=$codreg && timeout is null";
+        $conasis = mysqli_query($conexion, $strasis) or die('error en la consulta'.mysqli_connect_error());
+        $idasis= mysqli_fetch_assoc($conasis);
+        $id=$idasis["id_asistencia"];
+
+        if(is_null($id)){ // usuario registro entrada
+            $strregen = "INSERT INTO asistencia values('','$tiempo','',$codreg)";
+            mysqli_query($conexion,$strregen) or die('error en la consulta'.mysqli_connect_error());
+
+            return 0;
+
+        }else{ // usuario registro salida
+            $strregsa = "UPDATE asistencia SET timeout = '$tiempo' WHERE id_asistencia = $id";
+            mysqli_query($conexion,$strregsa) or die('error en la consulta'.mysqli_connect_error());
+
+            return 1;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+?>
--- post.php
+++ post.php
@@ -4,10 +4,12 @@
 	require 'funciones.php';
 
 	date_default_timezone_set("America/Bogota");
-	$dato = $_POST;
+	$valortag = $_POST;
+	$tiempo = date("y-m-d h:i:s");
 
-	$respuesta = registrarEntrada($dato["codigo"],date("y-m-d h:i:s"));
+	$resultado = registrar($valortag["codigo"],$tiempo);
 
+	//$user = obtenerUsuario($valortag["codigo"]);
 
 ?>
 
@@ -20,14 +22,14 @@
 				margin: 0;
 				padding: 0;
 				background: black;
+				color: white;
 			}
 
 			#usuario{
 				width: 300px;
 				margin:50 auto;
 				border: 1px solid #59df7a;
-				text-align: center;
-				color: white;
+				text-align: center;				
 
 			}
 
@@ -38,12 +40,18 @@
 
 		<div id="usuario">
 			<p>
-		        <?php if(!is_null($respuesta)){
-		        		echo "ENTRADA REGISTRADA"."</br>"
-		        			.$respuesta["cod_usuario"]." ".$respuesta["nombre"]." ".$respuesta["cargo"];
+		        <?php if($resultado != -1){
+		        			$usuario=obtenerUsuario($valortag["codigo"]);
+		        			if($resultado == 0){
+		        				echo "Entrada registrada </br>"
+		        					 .$tiempo." ".$usuario["cod_usuario"]." ".$usuario["nombre"]." ".$usuario["cargo"];
+		        			}else echo "Salida registrada </br>"
+		        					   .$tiempo." ".$usuario["cod_usuario"]." ".$usuario["nombre"]." ".$usuario["cargo"];
 		        		}else echo "Usuario no registrado";
 		        ?>
-	        </p>       
+	        </p>
+
+	        <!--<p><?php var_dump($user) ?></p>-->
 
 			<a href="index.html">Inicio</a>			
 
